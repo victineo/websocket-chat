26d9dd4f2530f590b1da0ed6382c2f1daa69e99e
Victor <victorhenrique.alves.lopes@gmail.com>
2025-01-18 16:48:18 -0300

    Removendo os códigos de 'TextInput' no repositório remoto, já que agora se chama 'TextArea

--- frontend/app/components/TextInput/TextInput.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './TextInput.module.css';
-
-export default function TextInput(
-    props: React.InputHTMLAttributes<HTMLInputElement>
-) {
-    return (
-        <input className={styles.textInput} {...props} />
-    );
-}
