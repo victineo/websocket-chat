7659a49243c2d4bb7ea48e17ad990b30344a4d0b
Victor <victorhenrique.alves.lopes@gmail.com>
2025-01-18 01:40:28 -0300

    Adicionando States, ícones, e tornando as interações do componente mais dinâmicas

--- frontend/app/components/MessageForm/MessageForm.tsx
+++ frontend/app/components/MessageForm/MessageForm.tsx
@@ -1,15 +1,45 @@
+import { useState } from 'react';
 import styles from './MessageForm.module.css';
 import TextInput from "../TextInput/TextInput";
+import { Send, Paperclip } from 'lucide-react';
 
-export default function MessageForm() {
+interface MessageFormProps {
+    onSendMessage: (message: string) => void;
+}
+
+export default function MessageForm({ onSendMessage }: MessageFormProps) {
+    const [messageText, setMessageText] = useState('');
+    console.log(setMessageText);
+
+    function handleMessageTextChange(event: React.ChangeEvent<HTMLInputElement>) {
+        event.target.setCustomValidity('');
+        setMessageText(event.target.value);
+    }
+
+    function handleSendMessage(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        if (!messageText.trim()) return;
+
+        onSendMessage(messageText);
+        setMessageText('');
+    }
+
+    const isMessageTextEmpty = messageText.length === 0;
+
     return (
-        <form className={styles.messageFormContainer}>
+        <form className={styles.messageFormContainer} onSubmit={handleSendMessage}>
             <TextInput
                 placeholder="Digite uma mensagem"
+                value={messageText}
+                onChange={handleMessageTextChange}
             />
             <div className={styles.messageFormButtons}>
-                <button type="button">Anexar arquivos</button>
-                <button type="submit">Enviar</button>
+                <button type="button" title="Anexar arquivos">
+                    <Paperclip size={24} />
+                </button>
+                <button type="submit" disabled={isMessageTextEmpty} title="Enviar mensagem">
+                    <Send size={24} />
+                </button>
             </div>
         </form>
     );
