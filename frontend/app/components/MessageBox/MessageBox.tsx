26715344310067ec48c29d8b0538f31e5e64d320
Victor <victorhenrique.alves.lopes@gmail.com>
2025-01-18 01:42:32 -0300

    Adicionando um texto que identifica o autor de uma mensagem

--- frontend/app/components/MessageBox/MessageBox.tsx
+++ frontend/app/components/MessageBox/MessageBox.tsx
@@ -9,6 +9,7 @@ export default function Message({ message, isOwnMessage }: MessageProps) {
     return (
         <div className={`${styles.messageContainer} ${isOwnMessage ? styles.messageContainerOwn : styles.messageContainerOther}`}>
             <div className={`${styles.messageBox} ${isOwnMessage ? styles.messageBoxOwn : styles.messageBoxOther}`}>
+                <h4 className={styles.messageAuthor}>{isOwnMessage ? 'Você' : 'Sistema'}</h4>
                 <p>{message}</p>
             </div>
         </div>
