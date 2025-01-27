f34b4876c69bba769986c028c3c6908cee222e45
Victor <victorhenrique.alves.lopes@gmail.com>
2025-01-27 18:18:54 -0300

    Implementando interface para passar os parâmetros do Aside e começando a estruturar interações de expandir e recolher

--- frontend/app/components/Aside/Aside.tsx
+++ frontend/app/components/Aside/Aside.tsx
@@ -1,16 +1,40 @@
+import { useState, Dispatch, SetStateAction } from 'react';
 import { PanelLeft, Home, LayoutGrid } from 'lucide-react';
 import styles from './Aside.module.css';
 
 import IconButton from '../IconButton/IconButton';
 import AsideMainButton from '../AsideMainButton/AsideMainButton';
+import ChatListItemButton from '../ChatListItemButton/ChatListItemButton';
 
-export default function Aside() {
+interface AsideProps {
+    setActiveSection: Dispatch<SetStateAction<string>>;
+}
+
+export default function Aside({ setActiveSection }: AsideProps) {
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
     return (
-        <aside className={styles.aside}>
-            <IconButton icon={<PanelLeft size={24} />} title="Recolher menu" />
+        <aside className={`${styles.aside} ${isCollapsed ? styles.collapsed : styles.expanded}`}>
+            <IconButton
+                icon={<PanelLeft size={24} />}
+                title={isCollapsed ? "Expandir menu" : "Recolher menu"}
+                onClick={() => setIsCollapsed(!isCollapsed)}
+            />
             <div className={styles.asideMainButtons}>
-                <AsideMainButton icon={<Home size={24} />} label="Início" title="Início" />
-                <AsideMainButton icon={<LayoutGrid size={24} />} label="Espaços" title="Espaços" />
+                <AsideMainButton 
+                    icon={<Home size={24} />} 
+                    label="Início" 
+                    title="Início"
+                    onClick={() => setActiveSection('home')}
+                />
+                <AsideMainButton 
+                    icon={<LayoutGrid size={24} />} 
+                    label="Espaços" 
+                    title="Espaços"
+                />
+            </div>
+            <div className={styles.chatList}>
+                <ChatListItemButton name='Chat de exemplo' />
             </div>
         </aside>
     );
