fff379c2327e452842e484172a9d47c8a408cc6e
Victor <victorhenrique.alves.lopes@gmail.com>
2025-01-27 18:17:26 -0300

    Implementando interface para passar os parâmetros do IconButton

--- frontend/app/components/IconButton/IconButton.tsx
+++ frontend/app/components/IconButton/IconButton.tsx
@@ -1,16 +1,21 @@
 import styles from './IconButton.module.css';
 import { ReactNode } from 'react';
 
+interface IconButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    icon: ReactNode;
+    title?: string;
+}
+
 export default function IconButton({
     icon,
     title
-}: {
-    icon: ReactNode;
-    title?: string;
-} & React.ButtonHTMLAttributes<HTMLButtonElement>) {
+}: IconButtonProps) {
     return (
-        <button className={styles.iconButton} title={title}>
-            <div className={styles.iconButtonStateLayer}>
+        <button
+            className={styles.iconButton}
+            title={title}
+        >
+            <div className={`${styles.iconButtonStateLayer}`}>
                 {icon}
             </div>
         </button>
