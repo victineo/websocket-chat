757569fe3af643650277669bce986280d99a0516
Victor <victorhenrique.alves.lopes@gmail.com>
2025-01-27 18:16:03 -0300

    Criando um componente de representação de um chat

---
+++ frontend/app/components/ChatListItemButton/ChatListItemButton.tsx
@@ -0,0 +1,17 @@
+import { Ellipsis } from 'lucide-react'
+import styles from './ChatListItemButton.module.css';
+
+export default function ChatListItemButton({
+    name
+}: {
+    name: string
+} & React.ButtonHTMLAttributes<HTMLButtonElement>) {
+    return (
+        <button className={`${styles.chatListItemButton}`}>
+            <div className={styles.chatListItemButtonStateLayer}>
+                <p>{name}</p>
+                <Ellipsis size={24} />
+            </div>
+        </button>
+    );
+}
