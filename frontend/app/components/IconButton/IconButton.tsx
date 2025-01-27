f4bd2ca837b6fa33f1fd83a7a315fe5fe7c800fb
Victor <victorhenrique.alves.lopes@gmail.com>
2025-01-27 01:07:25 -0300

    Corrigindo o arquivo de JSX para TSX

--- frontend/app/components/IconButton/IconButton.tsx
+++ frontend/app/components/IconButton/IconButton.tsx
@@ -1,6 +1,13 @@
 import styles from './IconButton.module.css';
+import { ReactNode } from 'react';
 
-export default function IconButton({ icon, title = '' }) {
+export default function IconButton({
+    icon,
+    title
+}: {
+    icon: ReactNode;
+    title?: string;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>) {
     return (
         <button className={styles.iconButton} title={title}>
             <div className={styles.iconButtonStateLayer}>
