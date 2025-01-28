42f6927e8326d92a891a3e45363fda8e104fa044
Victor <victorhenrique.alves.lopes@gmail.com>
2025-01-27 23:32:34 -0300

    Definindo estilizações para tamanhos diferentes e incluindo esse tamanho nos parâmetros do IconButton

--- frontend/app/components/IconButton/IconButton.tsx
+++ frontend/app/components/IconButton/IconButton.tsx
@@ -4,18 +4,22 @@ import { ReactNode } from 'react';
 interface IconButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     icon: ReactNode;
     title?: string;
+    size?: 'xs' | 'm';
 }
 
 export default function IconButton({
     icon,
-    title
+    title,
+    size = 'm',
+    ...props
 }: IconButtonProps) {
     return (
         <button
             className={styles.iconButton}
             title={title}
+            {...props}
         >
-            <div className={`${styles.iconButtonStateLayer}`}>
+            <div className={`${styles.iconButtonStateLayer} ${styles[`size${size.toUpperCase()}`]}`}>
                 {icon}
             </div>
         </button>
