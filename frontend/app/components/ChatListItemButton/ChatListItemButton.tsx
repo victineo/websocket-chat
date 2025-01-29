d69cbb1be9d8ed724fc928d07564563e65c6a74f
Victor <victorhenrique.alves.lopes@gmail.com>
2025-01-29 03:17:21 -0300

    Implementando interface para passar os parâmetros do ChatListItemButton e substituindo a tag Link por Button em sua estrutura HTML

--- frontend/app/components/ChatListItemButton/ChatListItemButton.tsx
+++ frontend/app/components/ChatListItemButton/ChatListItemButton.tsx
@@ -2,19 +2,23 @@ import { Ellipsis } from 'lucide-react'
 import styles from './ChatListItemButton.module.css';
 
 import IconButton from '../IconButton/IconButton';
-import Link from 'next/link';
 
-export default function ChatListItemButton({
-    name
-}: {
-    name: string
-}) {
+interface ChatListItemButtonProps {
+    name: string;
+    onClick?: () => void;
+}
+
+export default function ChatListItemButton({ name, onClick }: ChatListItemButtonProps) {
     return (
         <div className={styles.chatListItemButtonWrapper}>
             <div className={styles.chatListItemButtonStateLayer}>
-                <Link href="www.google.com" className={styles.chatListItemButton}>
+                <button 
+                    className={styles.chatListItemButton}
+                    onClick={onClick}
+                    title={name}
+                >
                     <p>{name}</p>
-                </Link>
+                </button>
                 <IconButton
                     size="xs"
                     icon={<Ellipsis />}
