712526a4a9cda6b6326d5d5a7e5ba5addbc70a24
Victor <victorhenrique.alves.lopes@gmail.com>
2025-02-05 21:54:10 -0300

    Removendo a responsabilidade da função 'handle_initial_message' de criar nomes para os novos chats; Criando uma função própria para lidar com o envio de mensagens automáticas do sistema; Implementando a lógica completa para armazenar mensagens no banco de dados local

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_socketio import SocketIO, emit
 #from flask_cors import CORS
 import random
+import time
 from controllers.controller_chat import ChatController
 
 app = Flask(__name__, template_folder='../frontend/templates')
@@ -38,8 +39,11 @@ def handle_initial_message(msg):
         user_id = request.sid
 
         # 1. Cria um novo chat
-        chat_id = ChatController.create_chat('Nome do Chat')
+        chat_id = ChatController.create_chat()
         print(f'Novo chat criado: {chat_id}')
+
+        chat = ChatController.get_chat(chat_id=chat_id)
+        chat_name = chat.name
 
         # 2. Adiciona a mensagem inicial ao novo chat
         # Objeto para ser passado ao controlador
@@ -53,35 +57,74 @@ def handle_initial_message(msg):
         # Objeto para ser retornado ao frontend
         message_data = {
             'chat_id': chat_id,
+            'chat_name': chat_name,
             'message': message, # Já inclui content e sender
             'timestamp': message['timestamp']
         }
-
         emit('new_initial_message', message_data, broadcast=True)
         print(f'Mensagem inicial recebida de {user_id}: {msg}')
+
+        # 4. Envia mensagem automática do sistema
+        send_system_message(chat_id)
     except Exception as e:
         print(f'Erro ao enviar mensagem inicial: {str(e)}')
 
 @socketio.on('chat_message')
-def handle_message(msg):
+def handle_chat_message(msg):
     try:
         user_id = request.sid
+        chat_id = msg.get('chat_id')
+
+        if not chat_id:
+            raise ValueError('Chat ID não fornecido')
+
+        # 1. Adiciona a mensagem ao chat
+        # Objeto para ser passado ao controlador
+        message = ChatController.add_message(
+            chat_id=chat_id,
+            content=str(msg['message']),
+            sender=user_id
+        )
+
+        # 2. Emite a mensagem com dados estruturados
+        # Objeto para ser retornado ao frontend
         message_data = {
-            'user_id': user_id,
-            'message': msg
+            'chat_id': chat_id,
+            'message': message, # Já inclui content e sender
+            'timestamp': message['timestamp']
         }
         emit('new_chat_message', message_data, broadcast=True)
         print(f'Mensagem recebida de {user_id}: {msg}')
-        
-        # Enviar mensagem automática de Lorem Ipsum
-        lorem_message_data = {
-            'user_id': 'Sistema',
-            'message': f'{random.choice(lorem_messages)}'
-        }
-        emit('new_chat_message', lorem_message_data, broadcast=True)
-        print(f'Mensagem automática enviada: {lorem_message_data["message"]}')
+
+        # 3. Envia mensagem automática de Lorem Ipsum
+        send_system_message(chat_id)
     except Exception as e:
         print(f'Erro ao processar mensagem: {e}')
+
+def send_system_message(chat_id):
+    try:
+        time.sleep(0.5) # Aguarda 0.5 segundos antes de enviar a mensagem automática
+
+        # Objeto para ser passado ao controlador
+        lorem_message = ChatController.add_message(
+            chat_id=chat_id,
+            content=random.choice(lorem_messages),
+            sender='system'
+        )
+
+        # Objeto para ser retornado ao frontend
+        lorem_message_data = {
+            'chat_id': chat_id,
+            'message': lorem_message, # Já inclui content e sender
+            'timestamp': lorem_message['timestamp']
+        }
+        emit('new_system_message', lorem_message_data, broadcast=True)
+        print(f'Mensagem automática enviada: {lorem_message_data["message"]["content"]}')
+
+        return lorem_message_data
+    except Exception as e:
+        print(f'Erro ao enviar mensagem automática: {e}')
+        return None
 
 
 if __name__ == '__main__':
