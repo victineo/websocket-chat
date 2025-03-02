080df712e6cb5dd8cd04cfec242e390b9131f35c
Victor <victorhenrique.alves.lopes@gmail.com>
2025-03-02 00:51:12 -0300

    Atualizando estrutura dos dados enviados para o FE

--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,8 @@ from flask_migrate import Migrate
 from flask_socketio import SocketIO, emit
 from database import db
 from models import *
+from controllers import *
 import random, time
-from controllers.controller_chat import ChatController
 
 app = Flask(__name__, template_folder='../frontend/templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -54,30 +54,26 @@ def handle_initial_message(msg):
         chat_doc_id = ChatController.create_chat(user_id)
         print(f'Novo chat criado: {chat_doc_id}')
 
-        chat = ChatController.get_chat(chat_doc_id)
-        chat_name = chat.name
-        chat_id = chat.id
-
         # 2. Adiciona a mensagem inicial ao novo chat
-        # Objeto para ser passado ao controlador
-        message = ChatController.add_message(
+        ChatController.add_message(
             chat_id=chat_doc_id,
             sender_id=user_id,
             content=msg
         )
 
-        # 3. Emite a mensagem com dados estruturados
-        # Objeto para ser retornado ao frontend
-        message_data = {
-            'chat_id': chat_id,
-            'chat_name': chat_name,
-            'message': message, # Já inclui content e sender
-            'timestamp': message['timestamp']
-        }
-        emit('new_initial_message', message_data, broadcast=True)
+        # 3. Busca o chat atualizado (agora com a nova mensagem)
+        chat = ChatController.get_chat(chat_doc_id).to_dict()
+        print(f'Estrutura da tabela Chat:\n{chat}')
+        messages = MessageController.get_chat_messages(chat_doc_id)
+        chat['messages'] = messages
+        print(f'Estrutura da tabela Message:\n{messages}')
+        print(f'Estrutura do dicionário de chat a ser enviado para o frontend:\n{chat}')
+
+        # 4. Emite o chat completo para o frontend
+        emit('new_initial_message', chat, broadcast=True)
         print(f'Mensagem inicial recebida de {user_id}: {msg}')
 
-        # 4. Envia mensagem automática do sistema
+        # 5. Envia mensagem automática do sistema
         send_system_message(chat_doc_id)
     except Exception as e:
         print(f'Erro ao enviar mensagem inicial: {str(e)}')
@@ -127,16 +123,10 @@ def send_system_message(chat_id):
             content=random.choice(lorem_messages)
         )
 
-        # Objeto para ser retornado ao frontend
-        lorem_message_data = {
-            'chat_id': chat_id,
-            'message': lorem_message, # Já inclui content e sender
-            'timestamp': lorem_message['timestamp']
-        }
-        emit('new_system_message', lorem_message_data, broadcast=True)
-        print(f'Mensagem automática enviada: {lorem_message_data["message"]["content"]}')
+        emit('new_system_message', lorem_message, broadcast=True)
+        print(f'Estrutura da mensagem automática enviada:\n{lorem_message}')
 
-        return lorem_message_data
+        return lorem_message
     except Exception as e:
         print(f'Erro ao enviar mensagem automática: {e}')
         return None
