1695e5a61c1485eb54cacd8c451129c668a40bd7
Victor <victorhenrique.alves.lopes@gmail.com>
2025-03-03 16:39:08 -0300

    Adicionando novo evento para lidar com as mensagens retornadas pelo BE; Aperfeiçoando estruturas de dados a serem usadas pelos estados do React

--- frontend/app/(pages)/page.tsx
+++ frontend/app/(pages)/page.tsx
@@ -21,16 +21,24 @@ export default function Home() {
     const [activeMessages, setActiveMessages] = useState<MessageData[]>([]);
 
     useEffect(() => {
+        console.log(`Estado atualizado de chats:`, chats);
+    }, [chats]);
+
+    useEffect(() => {
+        console.log(`Estado atualizado de activeChat:`, activeChat);
+    }, [activeChat]);
+    
+    useEffect(() => {
+        console.log(`Estado atualizado de activeMessages:`, activeMessages);
+    }, [activeMessages]);
+
+    useEffect(() => {
         const newSocket = io('http://localhost:5000');
 
         newSocket.on('connect', () => {
-            console.log('Conectado ao servidor');
-            console.log(`newSocket ID: ${newSocket.id}`);
+            console.log(`Conectado ao servidor com o ID: ${newSocket.id}`);
 
-            setSocket((prevSocket) => {
-                console.log(`Socket antigo: ${prevSocket?.id}`);
-                return newSocket;
-            });
+            setSocket(newSocket);
 
             newSocket.emit('get_initial_chats');
             console.log(`Solicitando chats para o backend...`);
@@ -44,36 +52,39 @@ export default function Home() {
                 return;
             }
 
-            console.log(`Chats recebidos do backend: ${initialChats.length}\n${JSON.stringify(initialChats, null, 2)}`);
+            console.log(`Chats recebidos do backend:`, initialChats);
 
             setChats(initialChats);
         });
 
         newSocket.on('new_initial_message', (data: any) => {
             console.log(`O backend acionou o evento 'new_initial_message'!`);
-            console.log(`Seção ativa: ${activeSection}`);
+
             if (activeSection === 'home') {
-                const newChat = {
+                const newChat: ChatData = {
                     id: data.id,
                     user_id: data.user_id,
                     space_id: data.space_id,
                     name: data.name,
-                    context_profile_id: data.context_profile_id,
-                    messages: data.messages.map((msg: any) => ({
-                        id: msg.id,
-                        chat_id: msg.chat_id,
-                        sender_id: msg.sender_id,
-                        content: msg.content,
-                        timestamp: msg.timestamp,
-                        isOwnMessage: msg.sender_id === socket?.id
-                    }))
+                    context_profile_id: data.context_profile_id
                 };
+
+                const newMessage: MessageData = {
+                    id: data.messages[0].id,
+                    chat_id: data.messages[0].chat_id,
+                    sender_id: data.messages[0].sender_id,
+                    content: data.messages[0].content,
+                    timestamp: data.messages[0].timestamp,
+                    isOwnMessage: data.messages[0].sender_id === socket?.id
+                }
+
+                console.log(`Metadados do Chat recebido:\n${JSON.stringify(newChat, null, 2)}`);
+                console.log(`Metadados da Mensagem recebida:\n${JSON.stringify(newMessage, null, 2)}`);
 
                 setActiveSection('chat');
                 setChats((prevChats) => [...prevChats, newChat]);
                 setActiveChat(newChat);
-                setActiveMessages(newChat.messages);
-                console.log(`Estrutura completa do novo chat criado:\n${JSON.stringify(newChat, null, 2)}`);
+                setActiveMessages((prevMessages) => [...prevMessages, newMessage]);
             }
         });
 
@@ -93,8 +104,6 @@ export default function Home() {
 
             setActiveMessages((prevMessages) => [...prevMessages, message]);
 
-            console.log(`Estrutura completa do chat ativo atualizada NO ESTADO:\n${JSON.stringify(activeChat, null, 2)}\nID: ${activeChat?.id}\nNome: ${activeChat?.name}`);
-
             console.log(`Mensagem recebida de ${message.sender_id}: ${message.content}`);
         });
 
@@ -111,9 +120,22 @@ export default function Home() {
             }
 
             setActiveMessages((prevMessages) => [...prevMessages, message]);
-
-            console.log(`Mensagem do sistema integrada ao chat: ${message.content}`);
         });
+
+        newSocket.on('chat_messages', (data: MessageData[]) => {
+            console.log(`O backend acionou o evento 'chat_messages'!`);
+            
+            const messages: MessageData[] = data.map((message: MessageData) => ({
+                id: message.id,
+                chat_id: message.chat_id,
+                sender_id: message.sender_id,
+                content: message.content,
+                timestamp: message.timestamp,
+                isOwnMessage: message.sender_id === socket?.id
+            }));
+
+            setActiveMessages(messages);
+        })
 
         setSocket(newSocket);
 
@@ -131,7 +153,6 @@ export default function Home() {
 
     function handleSendChatMessage(message: string) {
         if (socket && activeSection === 'chat' && activeChat) {
-            console.log(`Estrutura completa do chat ativo: ${JSON.stringify(activeChat, null, 2)}`);
             socket.emit('chat_message', {
                 chat_id: activeChat.id,
                 message: message
@@ -145,6 +166,11 @@ export default function Home() {
         }
 
         setActiveChat(chat);
+        console.log(`Estrutura do chat selecionado:`, chat);
+
+        if (socket) {
+            socket.emit('get_chat_messages', chat.id);
+        }
     }
 
     return (
