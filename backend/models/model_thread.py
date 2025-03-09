e7d7ffdf32b993a5a01943cb06404a5ed4abdd0a
Victor <victorhenrique.alves.lopes@gmail.com>
2025-03-09 01:58:00 -0300

    Melhorando visual do código

--- backend/models/model_thread.py
+++ backend/models/model_thread.py
@@ -3,6 +3,7 @@ import uuid
 
 class Thread(db.Model):
     __tablename__ = 'threads'
+    
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     chat_id = db.Column(db.String(36), db.ForeignKey('chats.id'), nullable=False)
     user_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
