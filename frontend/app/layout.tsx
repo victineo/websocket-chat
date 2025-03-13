2017412e72e1222b1e9b26430c4741a248dbb5ca
Victor <victorhenrique.alves.lopes@gmail.com>
2025-03-13 00:09:10 -0300

    Ajustando o layout para fazer com que useContext funcione em toda a aplicação

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ModalProvider } from "./context/ModalContext";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ModalProvider>
+          {children}
+        </ModalProvider>
       </body>
     </html>
   );
