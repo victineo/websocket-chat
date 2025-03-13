1ae355ae1ccb768a25ee657b59600c9aa5e0315f
Victor <victorhenrique.alves.lopes@gmail.com>
2025-03-13 00:10:48 -0300

    Começando a projetar a sobreposição de criação de novos Espaços

---
+++ frontend/app/components/ModalCreateSpace/ModalCreateSpace.tsx
@@ -0,0 +1,29 @@
+import styles from './ModalCreateSpace.module.css';
+import IconButton from '../IconButton/IconButton';
+import TextArea from '../TextArea/TextArea';
+import TextInput from '../TextInput/TextInput';
+import { X } from 'lucide-react';
+
+export default function ModalCreateSpace({ closeModal }: { closeModal: () => void }) {
+    return (
+        <div className={styles.modalCreateSpaceContainer}>
+            <div className={styles.modalHeader}>
+                <h2>Criar Espaço</h2>
+                <IconButton icon={<X size={24} />} title="Fechar" onClick={closeModal} />
+            </div>
+            <div className={styles.modalBody}>
+                <div className={styles.modalInputContainer}>
+                    <label htmlFor="name">Nome do espaço</label>
+                    <TextInput placeholder="Estudos" />
+                </div>
+                <div className={styles.modalInputContainer}>
+                    <label htmlFor="description">Descrição do espaço</label>
+                    <TextArea placeholder="Espaço para focar em aprendizagem" />
+                </div>
+            </div>
+            <div className={styles.modalFooter}>
+                <button type="submit">Criar</button>
+            </div>
+        </div>
+    );
+}
