db9f4edb67d19442e37ff6e3ef0123ef136913ab
Victor <victorhenrique.alves.lopes@gmail.com>
2025-03-13 00:08:22 -0300

    Aplicando contexto no SpaceCreateCard

--- frontend/app/components/SpaceCreateCard/SpaceCreateCard.tsx
+++ frontend/app/components/SpaceCreateCard/SpaceCreateCard.tsx
@@ -1,17 +1,23 @@
 import styles from './SpaceCreateCard.module.css';
-
 import { Plus } from 'lucide-react';
+import { useModal } from '../../context/ModalContext';
+import ModalCreateSpace from '../ModalCreateSpace/ModalCreateSpace';
 
 export default function SpaceCreateCard() {
+    const { showModal, hideModal } = useModal();
+
+    function handleOpenModal() {
+        showModal(<ModalCreateSpace closeModal={hideModal} />);
+    }
+
     return (
-        <div className={styles.spaceCreateCard}>
+        <div className={styles.spaceCreateCard} onClick={handleOpenModal}>
             <div className={styles.spaceCreateCardStateLayer}>
                 <div className={styles.spaceCreateCardIconContainer}>
                     <Plus size={24} />
                 </div>
                 <div className={styles.spaceCreateCardTextInfo}>
                     <h1>Criar um novo espaço</h1>
-                    {/* <p>Adicione um nome e uma descrição para o seu espaço</p> */}
                 </div>
             </div>
         </div>
